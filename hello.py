e1527a0b3962a30e355eaee4474b5100e9cd4db4
home <evan_rush@mail.ru>
2019-07-29 19:21:41 +0300

    two

--- hello.py
+++ hello.py
@@ -1,4 +1,2 @@
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+bind="0.0.0.0:8080"
+pythonpath="/home/box/web/"
