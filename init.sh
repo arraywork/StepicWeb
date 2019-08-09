de137cee622925f820fdc6782aec082050c4bcf1
home <evan_rush@mail.ru>
2019-08-09 22:20:03 +0300

    Архитектура frontend-backend

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-cd /home/box/web
-sudo gunicorn -c gunicorn.conf hello:app
+cd /home/box/web; sudo gunicorn - b 0.0.0.0:8080 hello:app
