2adeea8b7af09538c9ba6e72bc0177ba78a0c468
esse <work@example.com>
2019-08-12 17:55:33 +0300

    Создание Django приложения

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-sudo rm /etc/nginx/sites-enabled/default
+﻿sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd /home/box/web;  sudo gunicorn -c gunicorn.conf hello:app &
+cd /home/box/web/ask ; sudo gunicorn -c gunicorn.conf wsgi:app &
 cd /home/box/web/ask; python3 manage.py runserver
+
